e13b610d1d304b30eaef9cc4f65fa5e27592e741
islem2246 <islem6594@gmail.com>
2024-02-19 12:22:22 +0100

    finish

---
+++ script.js
@@ -0,0 +1,25 @@
+function colorchange(color){
+    document.body.style.background=color;
+};
+document
+  .getElementById("submitBtn")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent form submission
+    var nameInput = document.getElementById("name");
+    var emailInput = document.getElementById("email");
+    if (nameInput.value.trim() === "" || emailInput.value.trim() === "") {
+      //trim
+      alert("Please fill in all required fields.");
+    } else {
+      alert("Form submitted successfully!");
+    }
+  });
+// Image hover effect
+var image = document.getElementById("image");
+image.addEventListener("mouseover", function () {
+  image.src = "new_image.jpg";
+});
+image.addEventListener("mouseout", function () {
+  image.src = "image.jpg";
+});
+ 
